d1e490333cabe44fb755c016228aaa85f283098b
Gabriel Dias <gdiasb12@gmail.com>
2020-03-28 16:00:01 -0300

    TDD optimization

--- src/app/Http/Controllers/DocumentController.php
+++ src/app/Http/Controllers/DocumentController.php
@@ -7,66 +7,75 @@ use App\Document;
 
 class DocumentController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    	$this->middleware('auth');
+    }
+
     //Brigs all Documents
-	public function index()
-	{
-		return Document::latest()->get();
-	} 
+    public function index()
+    {
+    	return Document::latest()->get();
+    } 
 
 	//Show the Document based on the ID  
-	public function show(Document $document)
-	{
-		try {
-			
-			if ($document->path_file) {
-				$xml = simplexml_load_file($document->path_file);
+    public function show(Document $document)
+    {
+    	try {
+    		
+    		if ($document->path_file) {
+    			$xml = simplexml_load_file($document->path_file);
 
-				return response()->Json($xml);
+    			return response()->Json($xml);
 
-			} else {
-				return response()->Json('File not found!', 404);
-			}
-		} catch (Exception $e) {
-			return response()->Json('Ops! Error on open file.');			
-		}
-	}
+    		} else {
+    			return response()->Json('File not found!', 404);
+    		}
+    	} catch (Exception $e) {
+    		return response()->Json('Ops! Error on open file.');			
+    	}
+    }
 
     //Save Document file and Create a new record 
-	public function store()
-	{
-		$file = request()->file('xmlfile');
-
+    public function store()
+    {
+    	$file = request()->file('xmlfile');
 		//Use the time to set a new name to the file 
-		$fileName = time().'_'.$file->getClientOriginalName();
-		
+    	$fileName = time().'_'.$file->getClientOriginalName();
+    	
 		//Move the file to a storage folder
-		if ($file->storeAs('public/xml', $fileName)) {
+    	if ($file->storeAs('public/xml', $fileName)) {
 
 			//Create a new record 
-			$document = Document::create([
-				'filename' => $fileName
-			]);
+    		$document = Document::create([
+    			'filename' => $fileName
+    		]);
 
-			$msg = [
-				'title' => 'Sucesso!',
-				'text' => 'Document uploaded!',
-				'icon' => 'success'
-			];
-		}else{
-			$msg = [
-				'title' => 'Ops!',
-				'text' => 'Erro on upload the file!',
-				'icon' => 'error'
-			];
-		}
-		return redirect('/home')->with('message', $msg);
-	}
+    		$msg = [
+    			'title' => 'Sucesso!',
+    			'text' => 'Document uploaded!',
+    			'icon' => 'success'
+    		];
+    	}else{
+    		$msg = [
+    			'title' => 'Ops!',
+    			'text' => 'Erro on upload the file!',
+    			'icon' => 'error'
+    		];
+    	}
+    	return redirect('/home')->with('message', $msg);
+    }
 
 	//Delete the record and unlink the file as well
-	public function destroy(Document $document)
-	{
-		$document->delete();
+    public function destroy(Document $document)
+    {
+    	$document->delete();
 
-		return redirect('/home');
-	}
+    	return redirect('/home');
+    }
 }
--- src/tests/Feature/DocumentTest.php
+++ src/tests/Feature/DocumentTest.php
@@ -2,34 +2,93 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
+use App\Document;
 
 class DocumentTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, WithoutMiddleware;
 
     /** @test */
-    public function testDocumentUpload()
+    public function a_document_can_be_uploaded()
     {
-        Storage::fake('avatars');
 
-        $file = UploadedFile::fake()->image('avatar.jpg');
+        $this->withoutExceptionHandling();
 
-        $response = $this->json('POST', '/document', [
-            'file' => $file,
-        ]);
+        $file = UploadedFile::fake()->create('document_test.xml');
+
+        // Use the time to set a new name to the file 
+        $fileName = time().'_'.$file->getClientOriginalName();
+        
+        //Move the file to a storage folder
+        $response = $file->storeAs('public/xml', $fileName);
 
         // Assert the file was stored...
-        Storage::disk('public')->assertExists($file->hashName());
+        Storage::disk('public')->assertExists('xml/'.$fileName);
 
-        // Assert a file does not exist...
-        Storage::disk('public')->assertMissing('missing.jpg');
-        
-        $this->assertCount(1, Document::all());
+        //Delete the file
+        Storage::disk('public')->delete('xml/'.$fileName);
+
     }
 
+    /** @test */
+    public function a_document_can_be_stored()
+    {
+        $this->withoutExceptionHandling();
+        
+        $response = $this->post('/document', $this->data());
+
+        $response->assertRedirect('/home');       
+
+        $document = Document::first();
+        //Delete the file
+        $this->deleteFile($document->filename);
+    }
+
+    /** @test */
+    public function a_document_list_can_be_showed()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post('/document', $this->data());
+
+        $this->get('/document')->assertStatus(200);
+
+        $document = Document::first();
+        //Delete the file
+        $this->deleteFile($document->filename);
+    }
+
+    /** @test */
+    public function a_document_can_be_deleted()
+    {
+
+        $this->withoutExceptionHandling();
+
+        $this->post('/document', $this->data());
+
+        $this->assertCount(1, Document::all());
+
+        //Delete the record
+        $response =  Document::first()->delete();
+
+        $this->assertCount(0, Document::all());
+    }
+
+    public function data()
+    {
+        return ['xmlfile' => UploadedFile::fake()->create('document_test.xml')];
+    }
+
+    public function deleteFile($fileName)
+    {
+        //Delete the file
+        Storage::disk('public')->delete('xml/'.$fileName);
+    }
 }
